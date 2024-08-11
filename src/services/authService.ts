2075d3f8f3569416f998e9d874986d4b0422b90e
Anh Bui <anhbhpd08263@fpt.edu.vn>
2024-08-11 22:42:00 +0700

    update auth service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -5,7 +5,7 @@ const http = new Http();
 
 export const register = async (newAccount: IAccount) => {
   try {
-    const data = await http.post('/accounts', newAccount);
+    const data = await http.post('/register', newAccount);
     return data;
   } catch (error) {
     console.log(error);
